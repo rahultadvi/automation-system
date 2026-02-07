c7a98b0bf590db60c1a4eba7d4b613a2c3a7b183
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-07 17:10:57 +0530

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -14,6 +14,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }  
 });
 
 // Test Database Connection
