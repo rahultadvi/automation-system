49019bc0918abf078c7f85a5a35e43fc0a3741d5
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-12 12:41:00 +0530

    chage

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -10,14 +10,44 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+// export const sendVerificationEmail = async (email, link) => {
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: "Verify Email",
+//     html: `
+//       <h2>Verify your email</h2>
+//       <a href="${link}">Click here</a>
+//     `
+//   });
+// };
 export const sendVerificationEmail = async (email, link) => {
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to: email,
     subject: "Verify Email",
+
+    text: `Verify your email using this link: ${link}`,
+
     html: `
-      <h2>Verify your email</h2>
-      <a href="${link}">Click here</a>
+      <div style="font-family:Arial;padding:20px">
+        <h2>Email Verification</h2>
+        <p>Please click the button below:</p>
+
+        <a href="${link}" 
+           style="
+              display:inline-block;
+              padding:10px 20px;
+              background:#2563eb;
+              color:white;
+              text-decoration:none;
+              border-radius:5px;">
+           Verify Email
+        </a>
+
+        <p>If button doesn't work, copy this link:</p>
+        <p>${link}</p>
+      </div>
     `
   });
 };
