2b4643df4af08a678b7108d5bb3733cf279499eb
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-12 09:34:59 +0530

    final

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,20 +6,22 @@ dotenv.config();
 const { Pool } = pkg;
 
 
+const isProduction = process.env.NODE_ENV === "production";
 
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  connectionString: process.env.DATABASE_URL,
-      ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false  
-});
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+      }
+);
 
 // Test Database Connection
 pool.connect()
