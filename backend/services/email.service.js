f640b382fa6461c75ba5705e7fea7d6bae657779
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-13 14:05:29 +0530

    chang

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -1,15 +1,26 @@
 import nodemailer from "nodemailer";
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   },
+//   tls: {
+//     rejectUnauthorized: false
+//   }
+// });
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
