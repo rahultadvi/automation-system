6d5be7a1ae39eae949782e61d03c2c5fe30f53e8
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-15 11:53:02 +0530

    deshbord

--- backend/router/auth.routes.js
+++ backend/router/auth.routes.js
@@ -13,22 +13,5 @@ router.post("/logout", logoutUser);
 router.get("/users", authMiddleware, getAllUsers);
 router.post("/set-password", setPassword);
 
-app.get("/test-email", async (req, res) => {
-  try {
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "rahultadvi8143@gmail.com",
-      subject: "DIRECT TEST",
-      html: "<h1>Working?</h1>"
-    });
-
-    console.log("Response:", response);
-    res.json(response);
-
-  } catch (error) {
-    console.log("Error full:", error);
-    res.json(error);
-  }
-});
 
 export default router;
--- backend/router/invite.routes.js
+++ backend/router/invite.routes.js
@@ -1,12 +1,31 @@
 import express from "express";
-// import { inviteUser } from "../controllers/invite.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { inviteUser, verifyInviteEmail } from "../controllers/inviteUser.controller.js";
-// import { verifyInviteEmail } from "../controllers/auth.controller.js";
+import { Resend } from "resend";
 
 const router = express.Router();
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 router.post("/invite", authMiddleware, inviteUser);
 router.get("/verify-email", verifyInviteEmail);
+
+router.get("/test-email", async (req, res) => {
+  try {
+    const response = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "rahultadvi8143@gmail.com",
+      subject: "FINAL TEST",
+      html: "<h1>Working now?</h1>"
+    });
+
+    console.log("SUCCESS:", response);
+    res.json(response);
+
+  } catch (error) {
+    console.log("FULL ERROR:", error);
+    res.json(error);
+  }
+});
 
 export default router;
