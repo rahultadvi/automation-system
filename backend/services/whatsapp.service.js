290f4406d21437d563fb1f0669af1535d9d2d2c3
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-17 14:13:31 +0530

    chage

--- backend/services/whatsapp.service.js
+++ backend/services/whatsapp.service.js
@@ -4,17 +4,20 @@ import pool from "../config/db.js";
 export const sendWhatsAppMessage = async (phoneNumber, messageText) => {
   console.log("👉 Phone Number:", phoneNumber);
   console.log("👉 Message Text:", messageText);
-  const cred = await pool.query(
-    "SELECT * FROM whatsapp_credentials LIMIT 1"
-  );
-
+  // const cred = await pool.query(
+  //   "SELECT * FROM whatsapp_credentials LIMIT 1"
+  // );
+const cred = await pool.query(
+  "SELECT whatsapp_token, phone_number_id FROM whatsapp_credentials WHERE user_id = $1",
+  [userId]
+);
   if (!cred.rows.length) {
     throw new Error("WhatsApp credentials not found");
   }
 
-  const { whatsapp_token1, phone_number_id } = cred.rows[0];
+  const { whatsapp_token, phone_number_id } = cred.rows[0];
 
-    console.log("👉 Token:", whatsapp_token1);
+    console.log("👉 Token:", whatsapp_token);
   console.log("👉 Phone ID:", phone_number_id);
 
   const url = `https://graph.facebook.com/v18.0/${phone_number_id}/messages`;
@@ -33,7 +36,7 @@ const response = await axios.post(
   },
   {
     headers: {
-      Authorization: `Bearer ${whatsapp_token1}`,
+      Authorization: `Bearer ${whatsapp_token}`,
       "Content-Type": "application/json"
     }
   }
